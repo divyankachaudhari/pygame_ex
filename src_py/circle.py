ad4238c34a60b5922ed45186d0a7978c23809923
mcs222066 <110977292+mcs222066@users.noreply.github.com>
2022-11-02 23:26:56 +0530

    Added basic collision functions for circle

--- src_py/circle.py
+++ src_py/circle.py
@@ -11,3 +11,24 @@ class Circle:
         self.diameter = 2*self.r
         self.area = math.pi * (self.r**2)
         self.circumference = math.pi * self.diameter
+
+        def collidePoint(self, point):
+        return (self.x - point[0])** 2 + (self.y - point[1])**2 <= (self.r)**2
+
+
+    def collideCircle(self, circle2):
+        return (self.x - circle2.x)**2 + (self.y - circle2.y)**2 <= (self.r + circle2.r)**2
+
+
+    def collideRect(self, rect):
+        r_x = self.x
+        r_y = self.y
+        if (self.x < rect.x) :
+                r_x = rect.x
+        elif(self.x > rect.right) :
+                r_x = rect.right
+        if (self.y < rect.y) :
+                r_y = rect.y
+        elif (self.y > rect.bottom) :
+                r_y = rect.bottom
+        return (self.x - r_x)**2 + (self.y - r_y)**2 <= (self.r)**2
