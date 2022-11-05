3fdb437dcb9295f92d32bdc35df21c9736de36f9
mcs222066 <110977292+mcs222066@users.noreply.github.com>
2022-11-05 17:08:00 +0530

    added comment to see which tests to add

--- test/circle_test.py
+++ test/circle_test.py
@@ -3,4 +3,13 @@ import math
 import pygame
 from pygame import circle, rect
 class TestCircle(unittest.TestCase):
-    
+    """
+    test area
+    circumference
+    diameter
+    collidePoint
+    collideCircle
+    collideRect
+    copy, move, update, contains
+    union, union all
+    """
