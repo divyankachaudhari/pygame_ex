25188773aec7ace9f87082031d3d1e45cb0b0905
Omkar Shitole <omkarpopatshitole@gmail.com>
2022-11-06 13:54:06 +0530

    added test for collide rect

--- test/circle_test.py
+++ test/circle_test.py
@@ -31,3 +31,24 @@ class TestCircle(unittest.TestCase):
         c = Circle(0, 0, 1)
 
         self.assertEqual(c.diameter, 2.0)
+
+    def test_collideRect(self):
+        msgt = "Expected True, rect should collide here"
+        msgf = "Expected False, rect should not collide here"
+        # ====================================================
+        c = Circle(0, 0, 5)
+
+        r1 = Rect(2, 2, 4, 4)
+        r2 = Rect(10, 15, 43, 24)
+        r3 = Rect(0, 4.9999999999999, 4, 4)
+
+        # colliding single
+        self.assertTrue(c.collideRect(r1), msgt)
+
+        # not colliding single
+        self.assertFalse(c.collideRect(r2), msgf)
+
+        # barely colliding single
+        self.assertTrue(c.collideRect(r3), msgt)
+
+    
