7d9703d720ee978842dcd4136a0c85787e824602
mcs222066 <110977292+mcs222066@users.noreply.github.com>
2022-11-06 10:14:16 +0530

    added tests for area, diameter and circumference

--- test/circle_test.py
+++ test/circle_test.py
@@ -4,12 +4,30 @@ import pygame
 from pygame import circle, rect
 class TestCircle(unittest.TestCase):
     """
-    test area
-    circumference
-    diameter
+    test area - done
+    circumference - done
+    diameter - done
     collidePoint
     collideCircle
     collideRect
     copy, move, update, contains
     union, union all
     """
+
+    def test_area(self):
+        """Ensures the area is calculated correctly."""
+        c = Circle(0, 0, 1)
+
+        self.assertEqual(c.area, math.pi)
+
+    def test_circumference(self):
+        """Ensures the circumference is calculated correctly."""
+        c = Circle(0, 0, 1)
+
+        self.assertEqual(c.circumference, math.tau)
+
+    def test_diameter(self):
+        """Ensures the diameter is calculated correctly."""
+        c = Circle(0, 0, 1)
+
+        self.assertEqual(c.diameter, 2.0)
